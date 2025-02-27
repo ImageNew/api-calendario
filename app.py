3ce6ff46556c4ae9cb4f4e6a26a86c4015f68b8c
ImageNew <imagenewmarketing@gmail.com>
2025-02-27 16:07:59 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+# Simulação de eventos comemorativos
+eventos = [
+    {"data": "01-01", "evento": "Confraternização Universal"},
+    {"data": "25-12", "evento": "Natal"},
+    {"data": "07-09", "evento": "Independência do Brasil"},
+]
+
+@app.route('/eventos', methods=['GET'])
+def get_eventos():
+    return jsonify(eventos)
+
+@app.route('/eventos/<data>', methods=['GET'])
+def get_evento_por_data(data):
+    evento = [e for e in eventos if e["data"] == data]
+    if evento:
+        return jsonify(evento)
+    return jsonify({"erro": "Nen
